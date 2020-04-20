5db061d74a917377fbf9e2180cb1c79fa9f345bb
Hakan Cetinkaya <54768569+hcetinkaya-r@users.noreply.github.com>
2020-04-20 08:57:29 +0300

    Unix ile İşletim Sistemleri(Java ile)

--- process.java
+++ process.java
@@ -1,31 +1,31 @@
-// **** burayi kendi package bilginiz ile degistirin.
-// package uis;
-// ****
 
-import java.util.concurrent.Semaphore;
+package uisodev;
+
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
-public class process 
-{
-	// 1 boyutlu semaphore aslinda "mutex"dir, bu semaphore'u sayac degiskenini artirip azaltirken kullanacagiz 
+
+/**
+ *
+ * @author hakancetinkaya
+ */
+public class process {
+    
+    // 1 boyutlu semaphore aslinda "mutex"dir, bu semaphore'u sayac degiskenini artirip azaltirken kullanacagiz 
 	static Semaphore semaphore = new Semaphore(1);
 	static int sayac = 0;
-	
 	// main fonksiyonunda alacagiz bu iki degiskeni
 	static int kuyruk_uzunlugu;
 	static int islem_sayisi;
 
 	static class islem extends Thread {
-
 		String name = "";
-
 		public void isim_olustur(String s)
 		{
 			name = s;
 		}
-		
+                @Override
 		public void run() {
-
 			try {
 				System.out.println(name + " : izin alinmaya calisiliyor...");
 				
@@ -35,23 +35,23 @@ public class process
 				while(true)
 				{	
 					// **** mutex i isteyin
-					
+					semaphore.acquire();
 					// ****
 					
 					// kritik bolge
-					// bu asagida sayac in kac olduðuna baktik
+					// bu asagida sayac in kac olduï£¿una baktik
 					if(sayac < kuyruk_uzunlugu)
 					{
 						// eger sayac kuyruk_uzunlugu'ndan kucukse sayac i artirin 
 						// ****
-						
+						sayac++;
 						// ****
 						
 						// sayac'i artirdiktan sonra mutex'i birakin, baska islemler kuyruga girmeyi denesin
 						
 						// burada mutex'i birakin
 						// ****
-						
+						semaphore.release();
 						// ****
 						
 						break;
@@ -59,12 +59,12 @@ public class process
 					{
 						// once mutex i birakin
 						// ****
-						
+						semaphore.release();
 						// ****
 						
 						// sonra 1 saniye uyuyun
 						// ****
-						
+						Thread.sleep(1000);
 						// ****
 						
 						// dongu tekrar						
@@ -76,77 +76,64 @@ public class process
 				System.out.println(name + " : izni aldik");
 
 				try {
-
 					// islem isini yaptigini bildirsin
 					for (int i = 1; i <= 3; i++) 
 					{
-
 						System.out.println(name + " : islem yapiyorum " + i);
-
 						// 1 saniye uyu
 						Thread.sleep(1000);
 					}
-
 				} finally {
-
-					
 					System.out.println(name + " : izin birakiliyor...");
 					
 					// kuyruktan ciktigimiz icin sayac i tekrar dusurmemiz gerekecek
 					// bunun icin ;
-					
 					// once mutex i isteyin
 					// **** 
-					
+					semaphore.acquire();
 					// ****
-					
-					// sonra sayac i 1 azaltýn
+		
+					// sonra sayac i 1 azaltËn
 					// ****
-					
+					sayac--;
 					// ****
 					
 					// mutex'i birakin
 					semaphore.release();					
-					
-
 				}
-
 			} catch (InterruptedException e) {
-
 				e.printStackTrace();
-
 			}
-
 		}
-
 	}
 
-	public static void main(String[] args)
-	{
-		
-		System.out.println("Kuyruk uzunlugunu giriniz: ");
+    public static void main(String[] args) {
+        System.out.println("Kuyruk uzunlugunu giriniz: ");
 		Scanner in = new Scanner(System.in);
 		// bu iki yildizli "comment" arasina Scanner kullanarak kullanicidan isletim sisteminin size ayiracagi
 		// kuyruk uzunlugunu aliniz
 		// ****
-		
+		kuyruk_uzunlugu = in.nextInt();
 		// ****
 		
 		System.out.println("Toplam islem sayisini giriniz: ");
 		// bu iki yildizli "comment" arasina Scanner kullanarak kullanicidan toplam islem sayisini aliniz
 		// ****
-		
+		islem_sayisi = in.nextInt();
 		// ****
-		
-		
 		// islem_listesi isimli tanimlayacagimiz islemleri tutacagimiz diziyi tanimlayin
 		// ****
-		
+    
+		islem islem_listesi[] = new islem[islem_sayisi];
 		// ****
 		
 		// for dongusunde islem_listesindeki her objeyi "new islem()" ile tanimlayin 
 		// ****
-		
+		for(int i=0;i<islem_sayisi;i++) {
+                    
+                    islem_listesi[i] = new islem();
+                  
+                }
 		// ****
 		
 		// "islem_listesi" dizisindeki her islemin for dongusu icinde sirayla "isim_olustur" 
@@ -157,22 +144,18 @@ public class process
 			
 			// tam bu noktada kullanicidan "in.next()" fonksiyonu ile string alip, listede i. islemin ismini olusturun
 			// ****
-			
+                        islem_listesi[i].isim_olustur(in.next());
 			// ****
 		}
-		 
-		
+                System.out.println(islem_listesi.length);
+	
 		// for dongusu icinde tum islem "thread"lerini "start()" fonksiyonu ile baslatin
 		// ****
-		
+		for(int i=0;i<islem_listesi.length;i++) {
+                   islem_listesi[i].start();
+                }
 		// ****
-
-	}
-	
-	
-	
-	
-	
-	
-
+      
+    }
+    
 }
